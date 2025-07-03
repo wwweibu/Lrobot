2e8c37ba5a2d8691187a1c141fb899f8aee58c3c
weibu <663748426@qq.com>
2025-07-03 18:53:00 +0800

    fix:自动创建backup文件夹

--- lrobot/logic/data/backup.py
+++ lrobot/logic/data/backup.py
@@ -21,6 +21,8 @@ MONGO_CONTAINER_PATH = "/data/backup"
 async def backup_mysql():
     """备份sql"""
     date = datetime.date.today().isoformat()
+    backup_dir = "/app/storage/backup"
+    os.makedirs(backup_dir, exist_ok=True)
     backup_path = f"/app/storage/backup/mysql_{date}.sql"
     cmd = f"mysqldump -h mysql -P 3306 -u root lrobot_data > {backup_path}"
     proc = await asyncio.create_subprocess_shell(cmd)
@@ -29,6 +31,8 @@ async def backup_mysql():
 
 async def backup_mongo():
     date = datetime.date.today().isoformat()
+    backup_dir = "/app/storage/backup"
+    os.makedirs(backup_dir, exist_ok=True)
     out_path = f"/app/storage/backup/mongo_{date}"
     cmd = f'mongodump --uri="mongodb://mongodb:27017/lrobot_log" --out={out_path}'
     proc = await asyncio.create_subprocess_shell(cmd)
