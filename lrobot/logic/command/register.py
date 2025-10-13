42d0bfd56a05b20bcd223b8c0dff74a81b45eb48
weibu <663748426@qq.com>
2025-10-13 22:01:10 +0800

    feat:修改入会字段

--- lrobot/logic/command/register.py
+++ lrobot/logic/command/register.py
@@ -11,7 +11,7 @@ FIELD_PATTERNS = {
     "codename": r"代号[：:]?(?P<codename>[^，,]+)",
     "gender": r"性别[：:]?(?P<gender>男|女)",
     "grade": r"年级[：:]?(?P<grade>\d{2}(研|博)?)",
-    "major": r"专业[：:](?P<major>[^，,]+)",
+    "major": r"专业[：:]?(?P<major>[^，,]+)",
     "student_id": r"学号[：:]?(?P<student_id>20\d{11})",
     "phone": r"电话[：:]?(?P<phone>1\d{10})",
     "qq": r"qq[：:]?(?P<qq>\d{5,12})",
