d894b52aa4a0b0bd7ee52b345978bc2f32526010
weibu <663748426@qq.com>
2025-10-27 00:08:19 +0800

    feat:优化水群消息格式1

--- lrobot/logic/command/water.py
+++ lrobot/logic/command/water.py
@@ -16,12 +16,13 @@ async def water_send(msg: Msg):
         return
     count += 1
     now = time.time()
+
     if count >= 100 and now - last_send_time >= 1800:
         Msg(
             platform=msg.platform,
             kind=f"群聊发送",
             event="发送",
-            content="欢迎找小推[at:1326016706]或小推·人机版(me)入会。可以不用加好友直接私聊我，发送\n\n/入会\n\n对协会活动有疑问也可以找我发送'/常见问题'\n注意去掉引号，保留/\n注：仅支持固定指令，智能问答请找另一个推",
+            content=f"欢迎找小推[at:1326016706]或小推·人机版(me)入会。可以不用加好友直接私聊我，发送\n\n/入会\n\n对协会活动有疑问也可以找我发送'/常见问题'\n注意去掉引号，保留/\n注：仅支持固定指令，智能问答请找另一个推[图片:{path / 'storage/file/command/water.jpg'}]",
             seq=msg.seq,
             group=config["public"]["水群"][0]
         )
